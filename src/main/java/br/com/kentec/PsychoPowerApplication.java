85a097f41155fa15fffe9712920aaa3637860ff2
devken-kentec <kennedy_tomazete@outlook.com>
2026-06-24 21:08:36 -0300

    init

---
+++ src/main/java/br/com/kentec/PsychoPowerApplication.java
@@ -0,0 +1,13 @@
+package br.com.kentec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychoPowerApplication.class, args);
+	}
+
+}
